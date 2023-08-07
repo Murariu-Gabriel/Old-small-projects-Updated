1ec0efbb8c278d6a70b7b6b41e4e0baac1dce147
Gabriel <murariugabi23@gmail.com>
2023-08-07 17:20:37 +0300

    Remade half of the js and styles

--- solar_sistem_project/Updated/scripts/main.js
+++ solar_sistem_project/Updated/scripts/main.js
@@ -14,25 +14,9 @@ const flexItem = document.querySelector("flex-item")
 const mass = document.querySelector("#mass")
 const select = document.querySelector("select")
 
-main.style.display = "flex"
-body.style.backgroundImage = "url(./images/galaxy.gif)"
-header.style.textAlign = "center"
-header.style.padding = "30px"
-h1.style.color = "white"
-flexContainer.style.display = "flex"
-flexContainer.style.justifyContent = "center"
-flexContainer.style.padding = "30px 150px"
-flexContainer.style.background = "rgba(240,240,240, 0.2)"
-flexContainer.style.maxWidth = "900px"
-main.style.justifyContent = "center"
 
 
-p2.style.textAlign = "center"
-img.style.height = "250px"
-
-
-
-function wieghtCalc(mass, planet) {
+function weightCalc(mass, planet) {
 
    if(planet === "earth") {
         return mass * 9.8
@@ -55,118 +39,64 @@ function wieghtCalc(mass, planet) {
     } else if(planet === "venus") {
         return mass * 8.8
     } else {
-        console.log("esti PROST")
+        console.log("ERROR")
     }
    
 }
 
 
-
-button.addEventListener('click', () => {
-description.style.background = "rgba(240,240,240, 0.4)"
-flexContainer.style.alignItems = "center"
-flexContainer.style.justifyContent = "space-around"
-p2.textAlign = "center"
-p2.style.background = "rgba(240,240,240, 0.6)"
-p2.style.borderRadius = "50%"
+const decidePlanet = (select, mass) => {
+  img.src = `./images/${select}.png`
+  p1.textContent = `The weight of the object on the planet ${select.toUpperCase()}`
+  p2.textContent = `${Math.floor(weightCalc(mass, select))} N `
+}
 
 
+const planets = (select, mass) => {
+    console.log(select)
+    if ( mass.length != 0) {
 
-    function planets(select, mass){
-    if (select === "earth" && mass.length != 0) {
-        img.src = "./images/earth.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-        p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-        
-    } else if(select === "jupiter" && mass.length != 0) {
-        img.src = "./images/jupiter.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-        p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "mars" && mass.length != 0) {
-        img.src = "./images/mars.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-        p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "mercury" && mass.length != 0) {
-        img.src = "./images/mercury.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-         p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "moon" && mass.length != 0) {
-        img.src = "./images/moon.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-         p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "neptune" && mass.length != 0) {
-        img.src = "./images/neptune.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-         p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "pluto" && mass.length != 0) {
-        img.src = "./images/pluto.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-        p2.textContent = `${wieghtCalc(mass, select)}N `
-
-    } else if(select === "saturn" && mass.length != 0) {
-        img.src = "./images/saturn.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-         p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "uranus" && mass.length != 0) {
-        img.src = "./images/uranus.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-         p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
-
-    } else if(select === "venus" && mass.length != 0) {
-        img.src = "./images/venus.png"
-        p1.textContent = `The wieght of the object on the planet ${select.toUpperCase()}`
-         p2.textContent = `${Math.floor(wieghtCalc(mass, select))} N `
+        decidePlanet(select, mass)
 
     } else if (select === "none" && mass.length != 0){
-        flexContainer.style.justifyContent = "center"
+        // flexContainer.style.justifyContent = "center"
         img.src = ""
         p1.textContent = "PLEASE SELECT PLANET"
-        p2.style.background = ""
-        p2.style.borderRadius = "0%"
+        // p2.style.background = ""
+        // p2.style.borderRadius = "0%"
         p2.textContent = ""
 
     } else if (typeof(mass) === "string") {
-        flexContainer.style.justifyContent = "center"
+        // flexContainer.style.justifyContent = "center"
         img.src = ""
         p1.textContent = 'PLEASE ENTER MASS'
         p2.textContent = ""
     }
-    console.log(wieghtCalc(mass, select))
+    console.log(weightCalc(mass, select))
 }
 
- planets(select.value, mass.value)
+
+button.addEventListener('click', () => {
+// description.style.background = "rgba(240,240,240, 0.4)"
+// flexContainer.style.alignItems = "center"
+// flexContainer.style.justifyContent = "space-around"
+// p2.textAlign = "center"
+// p2.style.background = "rgba(240,240,240, 0.6)"
+// p2.style.borderRadius = "50%"
+
+
+ planets(select.value, parseInt(mass.value))
  
  console.log(p2.textContent)
+
  if (p2.textContent.includes("NaN")) {
-    flexContainer.style.justifyContent = "center"
-            img.src = ""
-            p1.textContent = 'Not A Number'
-            p2.textContent = ""
+    // flexContainer.style.justifyContent = "center"
+    img.src = ""
+    p1.textContent = 'Not A Number'
+    p2.textContent = ""
 }
 
 })
 
 
-
-
-// if (mass === "string") {
-//     flexContainer.style.justifyContent = "center"
-//         img.src = ""
-//         p1.textContent = 'Not A Number'
-//         p2.textContent = ""
-
-// for(let el of select){
-//    if(el.value === "jupiter") {
-//     img.src = "./images/jupiter.png"
-//     description.textContent = `The wieght of the object on the planet ${el.value.toUpperCase()}`
-//     flexContainer.style.alignItems = "center"
-//     flexContainer.style.justifyContent = "space-around"
-//    }
-// }
 
